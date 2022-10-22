b91ae1978ee5b4e55393bac3557933001650c461
unknown <keishaganaban@gmail.com>
2022-10-22 11:15:33 +0800

    Upload

---
+++ SoundMind/SoundMind/app/src/main/java/com/example/soundmind/SMHome.kt
@@ -0,0 +1,11 @@
+package com.example.soundmind
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SMHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_smhome)
+    }
+}
